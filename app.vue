b089f7d5ede191c708a87269c0e00988c532c2ed
Shemmy <Shemmynyirenda3@gmail.com>
2024-10-20 16:44:47 +0200

    full not ful

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
 </template>
 <style scoped>
 body {
-  @apply min-h-screen w-ful;
+  @apply min-h-screen w-full;
   @apply bg-colorBackgroundLight dark:bg-colorBackgroundDark text-colorTextLight dark:text-colorTextDark;
 }
 </style>
