4ce3162b7797e25391db4bd2d27562790bde11a6
Shemmy <Shemmynyirenda3@gmail.com>
2024-10-20 19:45:19 +0200

    added more foundational work

---
+++ Components/Container.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full md:max-w-6xl flex flex-col px-4 md:px-8">
+    <slot />
+  </div>
+</template>
--- app.vue
+++ app.vue
@@ -1,9 +1,22 @@
 <template>
-  <div></div>
+  <div class="w-full min-h-screen">
+    <!--Header -->
+    <header
+      class="fixed left-0 top-0 right-0 px-4 md:px-8 flex items-center shadow-sm bg-colorSurfaceLight dark:bg-colorSurfaceDark text-colorTextLight dark:text-colorTextDark"
+    >
+      <!-- Header clamp-->
+      <div
+        class="w-full md:max-w-6xl min-h-12 mx-auto flex items-center justify-between"
+      ></div>
+    </header>
+    <div class="mt-16">
+      <NuxtPage />
+    </div>
+  </div>
 </template>
-<style scoped>
+<style>
 body {
-  @apply min-h-screen w-full;
+  @apply min-h-full w-full flex flex-col;
   @apply bg-colorBackgroundLight dark:bg-colorBackgroundDark text-colorTextLight dark:text-colorTextDark;
 }
 </style>
